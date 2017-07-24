2de1cc0838d7bebbc27d2a4ed84d0ebd408c60ec
Rajko Albrecht <ral@alwins-world.de>
2017-07-24 23:31:57 +0200

    add method documentation

--- lib/redis/store_with_cas/namespace.rb
+++ lib/redis/store_with_cas/namespace.rb
@@ -1,6 +1,12 @@
 class Redis
   class Store < self
+    # Add namespaced methods missing in #Redis::Store
     module Namespace
+      # redis.watch for a list of keys respecting the namespace for keys of the
+      # Redis::Store instance
+      # @parameter keys Array a list of keys to watch
+      # @see Redis::Store::watch in redis-store-gem
+      # @see Redis::watch in redis-gem
       def watch *keys
         if keys.any?
           super(*keys.map {|key| interpolate(key) })
